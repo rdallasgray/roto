d64b72819ec768658287da786fc03a3c3b156644
Robert Dallas Gray <mail@robertdallasgray.com>
2011-09-14 15:30:48 +0100

    Changed to standard project structure

---
+++ Makefile
@@ -0,0 +1,34 @@
+PROJECT = Bez
+SRC_DIR = src
+BUILD_DIR = .
+MAIN = jquery.bez
+
+JS_ENGINE ?= `which node nodejs`
+COMPILER ?= `which uglifyjs`
+
+VERSION = $(shell git describe --tags --long | sed s/\-/\./)
+YEAR = $(shell date +"%Y")
+
+all: main min clean
+
+nomin: main clean
+
+main: version
+
+version:
+	@@echo "Setting version number (${VERSION}) and year (${YEAR})"
+	@@sed 's/@VERSION/${VERSION}/' <${SRC_DIR}/${MAIN}.js | sed 's/@YEAR/${YEAR}/' > ${BUILD_DIR}/${MAIN}.tmp
+
+
+min:
+	@@if test ! -z ${JS_ENGINE} && test ! -z ${COMPILER}; then \
+	echo "Minifying ${PROJECT}"; \
+	${COMPILER} ${SRC_DIR}/${MAIN}.js > ${BUILD_DIR}/${MAIN}.min.js; \
+	else \
+		echo "You must have NodeJS and UglifyJS installed in order to minify ${PROJECT}."; \
+	fi
+
+clean:
+	@@echo "Removing temp files"
+	@@rm -f ${BUILD_DIR}/${MAIN}.tmp
+	@@echo "Done"
--- src/jquery.roto.js
+++ src/jquery.roto.js
@@ -1,34 +1,12 @@
-/**
- * Roto 1.0
- *
- * A simple, flexible, touch-capable image spinner plugin for jQuery
+/*!
+ * Roto @VERSION
+ * http://github.com/rdallasgray/roto
  * 
- * This software (jquery.roto.1.0.js) is provided under the BSD license.
- *  
- * Copyright 2011 Robert Dallas Gray. All rights reserved.
+ * A simple, flexible, touch-capable scrolling plugin for jQuery
  * 
- * Redistribution and use in source and binary forms, with or without modification, are
- * permitted provided that the following conditions are met:
- * 
- *    1. Redistributions of source code must retain the above copyright notice, this list of
- *       conditions and the following disclaimer.
- * 
- *    2. Redistributions in binary form must reproduce the above copyright notice, this list
- *       of conditions and the following disclaimer in the documentation and/or other materials
- *       provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
+ * Copyright @YEAR Robert Dallas Gray. All rights reserved.
+ * Provided under the FreeBSD license: https://github.com/rdallasgray/roto/blob/master/LICENSE.txt
+*/
 (function($){
 	$.fn.roto = function(options) {
 		var defaults = {
