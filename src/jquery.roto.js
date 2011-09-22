ac01dad991dec9118ada66c2515b7e4414dd994f
Robert Dallas Gray <mail@robertdallasgray.com>
2011-09-22 17:02:29 +0100

    Improved snapping

--- src/jquery.roto.js
+++ src/jquery.roto.js
@@ -217,30 +217,57 @@
 				
 				// find the list element nearest the given offset, and its position
 				getNearestListItemTo = function(offset, dir) {
-					var _pos, pos = _pos = maxOffset,
+					var pos = maxOffset, extent, bound,
 						dir = (dir === 0) ? lastValidDir : dir,
 						lis = (dir > 0) ? listItems.get().reverse() : listItems,
 						li = listItems.get(0);
 					$.each(lis, function(idx, el) {
+						_el = $(el);
 						// set pos to the position of the current listItem
-						pos = -1 * Math.ceil($(el).position()[dimensions.offsetName]);
+						pos = -1 * Math.ceil(_el.position()[dimensions.offsetName]);
 						li = el;
+						if (dir < 0) {
+							extent = (-1 * pos) + _el["outer" + dimensions.measure](true);
+							bound = -1 * offset;
+						}
+						else {
+							extent = _el.prev().length > 0 ? pos + _el.prev()["outer" + dimensions.measure](true) : pos;
+							bound = offset;
+						}
 						// if the position is beyond the offset, break the loop
-						if (pos * dir >= offset * dir) {
+						if (pos * dir >= offset * dir || extent > bound) {
 							return false;
 						}
 					});
 					return [li, pos];
 				},
 				
+				// find the next (by direction) listitem to the given offset
+				getNextListItemTo = function(offset, dir) {
+					var dir = (dir === 0) ? lastValidDir : dir,
+						func = dir < 0 ? "next" : "prev",
+						curr = getNearestListItemTo(offset, dir)[0],
+						next = $(curr)[func](),
+						li = next.length > 0 ? next : $(curr);
+					return li;
+				},
+				
 				// get the position of the listitem nearest the given offset
-				getSnapMove = function(offset, dir) {
-					return getNearestListItemTo(offset, dir)[1];
+				getSnapMove = function(offset, dir, next) {
+					return next ? 
+						-1 * Math.ceil(getNextListItemTo(offset, dir).position()[dimensions.offsetName]) 
+						: getNearestListItemTo(offset, dir)[1];
+				},
+				
+				// is the roto already snapped to a listitem?
+				isSnapped = function() {
+					var cOffset = getCurrentOffset();
+					return cOffset === getSnapMove(cOffset, 0, false);
 				},
 				
 				// trigger event on completion of move
 				notifyChanged = function() {
-					container.trigger("rotoChange", [getNearestListItemTo(getCurrentOffset(), lastValidDir)[0]]);
+					container.trigger("rotoChange", [getNearestListItemTo(getCurrentOffset(), 1)[0]]);
 					changeEventPrimed = false;
 				},
 				
@@ -250,7 +277,7 @@
 					containerMeasure = Math.ceil(ul.parent()[dimensions.measure.toLowerCase()]()),
 					minOffset = Math.ceil(rotoMeasure - containerMeasure + offsetCorrection + options.endOffset) * -1;
 					if (options.snap) {
-						minOffset = getSnapMove(minOffset, -1);
+						minOffset = getSnapMove(minOffset, -1, false);
 					}
 				},
 				
@@ -286,6 +313,7 @@
 					if (running) return;
 					running = true;
 					setTransitions();
+					lastValidDir = dir;
 
 					// internal function to move the listitems by the calculated amount
 					var doShift = function(move) {
@@ -297,11 +325,11 @@
 
 					if (dir < 0) {
 						// if we're moving forwards, find the element nearest the end of the container
-						move = Math.max(getSnapMove(getCurrentOffset() - containerMeasure, dir), minOffset);
+						move = Math.max(getSnapMove(getCurrentOffset() - containerMeasure, dir, false), minOffset);
 					}
 					else {
 						// if we're moving backwards, find the element one container width towards the start of the container
-						move = Math.min(getSnapMove(getCurrentOffset() + containerMeasure, dir), maxOffset);
+						move = Math.min(getSnapMove(getCurrentOffset() + containerMeasure, dir, false), maxOffset);
 					}
 					// move the offsetElement to the start of the container
 					doShift(move);
@@ -319,21 +347,21 @@
 				// continue ul movement inertially based on pointer speed
 				rotoDrift = function() {
 					var speed_dir = timer.getPointerSpeed(),
-						speed = speed_dir[0], dir = speed_dir[1];
-					if (speed === 0) {
-						switchButtons();
-						if (!options.snap) {
-							if ((!changeEventPrimed || transitionProp === null) && trackingOffset !== getCurrentOffset()) notifyChanged();
-							return;
-						};
-					}
+						speed = speed_dir[0], dir = speed_dir[1],
+						cOffset = getCurrentOffset();
+						
 					// distance to rotoDrift
-					var distance = speed * options.drift_factor * dir,
-						move = distance + getCurrentOffset();
+					var distance = speed * options.drift_factor * dir, 
+						move = distance + cOffset;
 					if (move > maxOffset) move = maxOffset;
 					else if (move < minOffset) move = minOffset;
-					else if (options.snap) move = getSnapMove(move, dir);
-				
+					else if (options.snap && !isSnapped()) {
+						move = getSnapMove(move, dir, true);
+					}
+					if (move === cOffset) {
+						notifyChanged();
+						return;
+					}
 					doAnimation(ul, getAnimatedProp(move), options.drift_duration, "drift", function() {
 						switchButtons();
 					});
